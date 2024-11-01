b527c6ac6ee612b826da6b8c615b853d8267c833
zhaoxinKun <zhaoxinkun1124@gmail.com>
2024-11-01 18:09:55 +0800

    110103

--- src/views/home/approvalProcess/firstInstance/index.vue
+++ src/views/home/approvalProcess/firstInstance/index.vue
@@ -143,9 +143,9 @@ export default {
         <el-pagination
             @size-change="handleSizeChange"
             @current-change="handleCurrentChange"
-            :current-page="currentPage4"
-            :page-sizes="[100, 200, 300, 400]"
-            :page-size="100"
+            :current-page="listQuery.pageNo"
+            :page-sizes="[10, 20, 30, 40]"
+            :page-size="listQuery.pageSize"
             layout="total, sizes, prev, pager, next, jumper"
             :total="rows">
         </el-pagination>
